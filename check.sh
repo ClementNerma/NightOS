3c202f751879616dfbe4a6c857562e3bd0beef82
Clément Nerma <clement.nerma@gmail.com>
2022-09-13 06:44:16 +0200

    Add a 'check' script

---
+++ check.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if ! command -v cargo > /dev/null
+then
+    echo "Command 'cargo' was not found, please install the Rust toolchain:"
+    echo "> https://rustup.rs/"
+    exit 1
+fi
+
+if ! command -v broken-md-links > /dev/null
+then
+    echo "Installing 'broken-md-links'..."
+
+    if ! cargo install broken-md-links
+    then
+        echo "Failed to install 'broken-md-links'"
+        exit 2
+    fi
+
+    echo "Done!"
+fi
+
+if ! broken-md-links -r docs/
+then
+    echo "Broken link(s) found, aborting commit."
+    exit 3
+fi
